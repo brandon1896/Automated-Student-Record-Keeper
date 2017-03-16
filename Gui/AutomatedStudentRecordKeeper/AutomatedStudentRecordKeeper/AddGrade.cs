c2e63d7b802de4ebf484478d118c9153932f0ac8
Shawn Anderson <anderson_19_21@hotmail.com>
2017-03-16 17:58:45 -0400

    Import html complete

--- Gui/AutomatedStudentRecordKeeper/AutomatedStudentRecordKeeper/AddGrade.cs
+++ Gui/AutomatedStudentRecordKeeper/AutomatedStudentRecordKeeper/AddGrade.cs
@@ -101,9 +101,12 @@ namespace AutomatedStudentRecordKeeper
                     sSelectedFile = choofdlog.FileName; //sets path
                     string html = System.IO.File.ReadAllText(sSelectedFile); //reads from path
 
+                    //add a check to make sure html is valid file
+                    //for example... if it starts with lakehead? regex.match
+
                     //clean file here//
                     string result = Regex.Replace(html, @" ?\<.*?\>", string.Empty); //removes everything between <>
-                    result = result.Substring(result.IndexOf("Name")); //removes irrelevant info at beginning of html file
+                    result = result.Substring(result.IndexOf("Name:")); //removes irrelevant info at beginning of html file
                     result = result.Substring(0, result.LastIndexOf("Term Average:")); //removes irrelevant info at bottom of html file
                     result = Regex.Replace(result, @"(Degree/CCD:).*?(?<=END OF)", string.Empty, RegexOptions.Singleline);
                     result = Regex.Replace(result, @"INTERNAL USE ONLY", string.Empty); //this line causes issues with courseCodeList values because of ONLY
@@ -114,7 +117,16 @@ namespace AutomatedStudentRecordKeeper
 
                     result = Regex.Replace(result, @"(name:)\s+", "Name: ", RegexOptions.IgnoreCase); //puts student name on same line
                     result = Regex.Replace(result, @"(student number:)\s+", "Student Number: ", RegexOptions.IgnoreCase); //puts student number on same line
+                    result = Regex.Replace(result, @"(year level:)\s+", "Year Level: ", RegexOptions.IgnoreCase); //puts year level on same line
                     string[] students = result.Split(new string[] { "Name:" }, StringSplitOptions.RemoveEmptyEntries);
+
+                    //grab student names
+                    var studentName = Regex.Matches(result, @"(?<=Name: ).*?(?<=\n)", RegexOptions.Singleline); //extracts student name
+                    var studentNameList = studentName.Cast<Match>().Select(match => match.Value).ToList();
+
+                    //grabs year level
+                    var yearLevel = Regex.Matches(result, @"(?<=Year Level: ).*?(?<=\n)", RegexOptions.Singleline);
+                    var yearLevelList = yearLevel.Cast<Match>().Select(match => match.Value).ToList();
 
                     for (int j = 0; j < students.Length; j++)
                     {
@@ -123,8 +135,7 @@ namespace AutomatedStudentRecordKeeper
                         var studentNumber = Regex.Matches(students[j], @"(?<=Student Number: )\d+"); //extracts student number --needs tweaking--                       
                         var studentNumberList = studentNumber.Cast<Match>().Select(match => match.Value).ToList();
 
-                        //drops table
-                        //seems to be working now
+                        //drops table if exists
                         try
                         {
                             cmd = new NpgsqlCommand("drop table \"" + studentNumberList[0] + "\"", conn);
@@ -138,19 +149,15 @@ namespace AutomatedStudentRecordKeeper
                         cmd = new NpgsqlCommand("create table \"" + studentNumberList[0] + "\"(coursesubject text, coursenumber text, coursename text, coursegrade integer, yearsection text, year integer)", conn);
                         cmd.ExecuteNonQuery();
 
-                        //Seems to double the time... consider other options...
-                        //var studentName = Regex.Matches(students[j], @".*?(Student Number:)", RegexOptions.Singleline); //extracts student name
-                        //var studentNameList = studentName.Cast<Match>().Select(match => match.Value).ToList();
-                        // -- still has "Student Number:" in it??? Whyyyyyy... FIX IT
-                        // -- NEED TO ADD STUDENT TO STUDENT TABLE!!!
-                        //System.IO.File.AppendAllLines(@"C:\Users\Shawn\Documents\studentNames.txt", studentNameList); //writes to text file
-
-                        //trying to grab year level -- only one year level on transcript --NOT WORKINGNGNGNSKGNDSKJGNSDKGNDS
-                        //var yearLevel = Regex.Matches(students[j], @"(?<=Year Level: )\s\d\s", RegexOptions.Singleline);
-                        //var yearLevelList = yearLevel.Cast<Match>().Select(match => match.Value).ToList();
-                        //System.IO.File.AppendAllLines(@"C:\Users\Shawn\Documents\yearLevel.txt", yearLevelList); //writes to text file
-
-                        Debug.Write("Files Length: " + files.Length);
+                        // to add student to students table as well
+                        cmd = new NpgsqlCommand("insert into student (name, studentid, yearlevel, yearsection, year) select :name, :num, :yrlvl, :yrsec, :entyear " +
+                            "WHERE NOT EXISTS (SELECT studentid FROM student WHERE studentid = '" + studentNumberList[0] + "') LIMIT 1; ", conn);
+                        cmd.Parameters.Add(new NpgsqlParameter("name", studentNameList[j]));
+                        cmd.Parameters.Add(new NpgsqlParameter("num", studentNumberList[0]));
+                        cmd.Parameters.Add(new NpgsqlParameter("yrlvl", int.Parse(yearLevelList[j])));
+                        cmd.Parameters.Add(new NpgsqlParameter("yrsec", (DateTime.Now.Year - (files.Length - 1)).ToString() + "/" + (DateTime.Now.Year - (files.Length - 2)).ToString()));
+                        cmd.Parameters.Add(new NpgsqlParameter("entyear", DateTime.Now.Year - (files.Length - 1)));
+                        cmd.ExecuteNonQuery();
 
                         for (int i = 0; i < files.Length; i++) //doesnt seem to be going through all files (possibly fixed)
                         {
@@ -158,32 +165,6 @@ namespace AutomatedStudentRecordKeeper
                             Debug.Write("Number of Students: j: " + j);
 
                             {
-                                //probably not needed --> most likely not needed
-                                /*
-                                cmd = new NpgsqlCommand("select exists (select true from pg_tables where tablename = '" + studentNumberList + "')", conn); //studentNumberList[0] when re-adding create table
-                                string checknum = "f";
-                                NpgsqlDataReader reader = cmd.ExecuteReader();
-                                while (reader.Read())
-                                {
-                                    checknum = reader[0].ToString();
-                                }
-                                cmd.Cancel();
-                                reader.Close();
-                                if (checknum == "False")
-                                */
-                                //{
-                                //write to dB
-
-                                /* -- this probably needs tweaking --
-                                //trying to grab year level -- only one year level on transcript
-                                var yearLevel = Regex.Matches(files[i], @"(?<=Year Level: )\s[0-9]\s", RegexOptions.Singleline);
-                                var yearLevelList = yearLevel.Cast<Match>().Select(match => match.Value).ToList();
-
-                                //trying to grab dates
-                                var yearDate = Regex.Matches(files[i], @"(?<=Fall/Winter)\s\d+/\d+\s|(?<=Spring/Summer)\s\d+\s", RegexOptions.Singleline);
-                                var yearDateList = yearLevel.Cast<Match>().Select(match => match.Value).ToList();
-                                */
-
                                 //trying to grab all uppercase 4 letter words, seems to work as planned --132 is the magic number
                                 var courseCode = Regex.Matches(files[i], @"\s[A-Z]{4}\s");
                                 var courseCodeList = courseCode.Cast<Match>().Select(match => match.Value).ToList();
@@ -196,16 +177,8 @@ namespace AutomatedStudentRecordKeeper
                                 var studentGrade = Regex.Matches(files[i], @"\s[0-9]{2,3}\s|\s(IP)\s");
                                 var studentGradeList = studentGrade.Cast<Match>().Select(match => match.Value).ToList();
 
-                                //if (courseCodeList.Count == courseNumberList.Count && courseCodeList.Count == studentGradeList.Count)
                                 int count = courseCodeList.Count;
-                                Debug.Write("Number of Courses: " + count);
 
-                                //System.IO.File.WriteAllText(@"C:\Users\Shawn\Documents\test" + i + ".txt", files[i]); //writes to text file
-                                //string year = files[i]; //need to convert to date? spring and summer courses cause an issue here
-
-                                Debug.Write("j" + j);
-
-                                //consider bulk insert, may be quicker (though this executes quickly as is)
                                 for (int n = 0; n < count; n++) //need the list size
                                 {
                                     string cSubject = courseCodeList[n];
@@ -218,38 +191,30 @@ namespace AutomatedStudentRecordKeeper
 
                                     Debug.Write("Attempting to write to DB");
 
-                                    //write to dB --this is questionable--
-                                    cmd = new NpgsqlCommand("insert into \"" + studentNumberList[0] + "\"(coursesubject,coursenumber,coursegrade) values(:sub, :num, :grade)", conn);
+                                    //write to dB
+                                    cmd = new NpgsqlCommand("insert into \"" + studentNumberList[0] + "\"(coursesubject,coursenumber,coursegrade, yearsection, year) values(:sub, :num, :grade, :yrsec, :yr)", conn);
                                     cmd.Parameters.Add(new NpgsqlParameter("sub", cSubject));
                                     cmd.Parameters.Add(new NpgsqlParameter("num", cNumber));
                                     if (sGrade == 0)
                                         cmd.Parameters.Add(new NpgsqlParameter("grade", DBNull.Value));
                                     else
                                         cmd.Parameters.Add(new NpgsqlParameter("grade", sGrade));
-                                    cmd.Parameters.Add(new NpgsqlParameter("yrsec", DBNull.Value));
-                                    cmd.Parameters.Add(new NpgsqlParameter("yr", DBNull.Value));
-                                    cmd.ExecuteNonQuery(); //currently broken!!!!!!!!!!!!!!!
+
+                                    cmd.Parameters.Add(new NpgsqlParameter("yrsec", (DateTime.Now.Year - ((files.Length - i))).ToString() + "/" + (DateTime.Now.Year - (files.Length - i) + 1).ToString()));
+                                    cmd.Parameters.Add(new NpgsqlParameter("yr", DateTime.Now.Year - (files.Length - i)));
+
+                                    cmd.ExecuteNonQuery();
 
                                 }
-                                /*
-                                //delete duplicates from table --might work? -- not working --syntax error
-                                //wont need if we keep the drop and recreate table
-                                //keep just in case for now
-                                cmd = new NpgsqlCommand("delete from " + studentNumberList[0] +
-                                    "where id in (select id " +
-                                    "from(SELECT id, ROW_NUMBER() OVER(partition BY column1, column2," +
-                                    " column3 ORDER BY id) AS rnum FROM " + studentNumberList[0] + ") t WHERE t.rnum > 1)", conn);
-                                cmd.ExecuteNonQuery();
-                                */
-                                //}
                             }
                         }
                     }
-
+                    MessageBox.Show("All students Added Succesfully. Import Complete.");
                 }
                 else
                     sSelectedFile = string.Empty;
             }
+            conn.Close();
         }
 
     }
